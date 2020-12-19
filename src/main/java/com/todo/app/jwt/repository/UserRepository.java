6083641fd945b6d8b664a2420e395c29c2eab0b9
Juan Sánchez <juan.sanchez@bitingbit.com>
2020-12-19 19:48:03 +0000

    1 - Create User-Roles entities and repositories

---
+++ src/main/java/com/todo/app/jwt/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.todo.app.jwt.repository;
+
+import com.todo.app.jwt.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
