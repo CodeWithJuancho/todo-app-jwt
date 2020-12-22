cb3d8770ee604ed98914a8b55fac5258221c5030
Juan Sánchez <juan.sanchez@bitingbit.com>
2020-12-22 19:08:31 +0000

    Add some comments

--- src/main/java/com/todo/app/jwt/util/SecurityConstants.java
+++ src/main/java/com/todo/app/jwt/util/SecurityConstants.java
@@ -3,5 +3,5 @@ package com.todo.app.jwt.util;
 public class SecurityConstants {
 
     public static final String TOKEN_PREFIX = "Bearer";
-    public static final long EXPIRATION_TIME = ((60 * 60) * 24) * 10;
+    public static final long EXPIRATION_TIME = ((60 * 60) * 24) * 10; // 864_000 seconds +> 10 days
 }
